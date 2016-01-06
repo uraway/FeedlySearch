0eccd365b483c0c51b15f7df18cb254087c7d419
uraway <masato.u0107@gmail.com>
2016-01-06 23:02:04 +0900

    console output

--- routes/feedly.js
+++ routes/feedly.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
   var req = require('request');
   req.get(url, function(error, response, body) {
     if (!error && response.statusCode == 200) {
-
+      console.log(body);
     } else {
       console.log(response.statusCode);
     }
