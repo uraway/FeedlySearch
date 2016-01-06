5b08be20e447e474f1957dfff2cbc5b826833e33
uraway <masato.u0107@gmail.com>
2016-01-06 22:58:51 +0900

    first commit

---
+++ routes/feedly.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.send('feedly');
+
+  var url = "http://cloud.feedly.com/v3/search/feeds?query=javascript";
+
+  var req = require('request');
+  req.get(url, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+
+    } else {
+      console.log(response.statusCode);
+    }
+  });
+});
+
+module.exports = router;
